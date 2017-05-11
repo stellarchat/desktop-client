1c9f32eaf69c416f84fe14490a6f3dc6806881bd
randolf-pc <lr@ripplefox.com>
2017-05-11 14:10:36 +0800

    change anchor to anchor_name

--- src/js/gateways.js
+++ src/js/gateways.js
@@ -4,6 +4,10 @@ var gateways = {
 			name : 'ripplefox.com',
 			website : 'https://ripplefox.com/anchor/',
 			deposit_api : true,
+			withdraw : [
+			            {type: 'alipay',   name: 'alipay'},
+			            {type: 'unionpay', name: 'bank'}
+			],
 			withdraw_info : 'Send CNY to Alipay: alipay/account/name*ripplefox.com. Send CNY to Bank: BankName/account/name*ripplefox.com',
 			withdraw_info_cn : '支付宝发送CNY到：zfb/账号/姓名*ripplefox.com，银行卡发送CNY到: 银行名称/账号/姓名*ripplefox.com',
 			assets : { 
@@ -15,6 +19,9 @@ var gateways = {
 			name : 'naobtc.com',
 			website : 'https://naobtc.com/',
 			deposit_api : true,
+			withdraw : [
+			            {type: 'bitcoin',   name: 'btc_withdraw'},
+			],
 			withdraw_info : 'Just input 1BTCAddressYouProvided*naobtc.com in the send page. You can just input the bitcoin address if naobtc.com is the default bitcoin service.',
 			withdraw_info_cn : '在发送页面输入“1BTCAddressYouProvided*naobtc.com”即可。如果naobtc.com是默认的比特服务，只要输入比特地址即可。',
 			assets : {
