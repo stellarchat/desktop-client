52d67f332030980ae11807fa338061d21a0858b4
Valters Grisans <valters.grisans@gmail.com>
2018-07-09 16:14:37 +0300

    Network pick on first launch

--- src/js/controller/header.js
+++ src/js/controller/header.js
@@ -1,10 +1,11 @@
 /* global myApp */
 
-myApp.controller("HeaderCtrl", ['$scope', '$route', '$rootScope', '$location', 'AuthenticationFactory', 'StellarApi', function( $scope , $route,  $rootScope ,  $location ,  AuthenticationFactory ,  StellarApi ) {
+myApp.controller("HeaderCtrl", ['$scope', '$route', '$window', '$rootScope', '$location', 'AuthenticationFactory', 'StellarApi', function( $scope , $route, $window,  $rootScope ,  $location ,  AuthenticationFactory ,  StellarApi ) {
 
     $scope.isActive = function(route) {
       return route === $location.path();
     }
+    $scope.launched = $window.localStorage['launched'] ? true : false;
 
     $scope.logout = function () {
       AuthenticationFactory.logout();
